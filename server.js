809286972e1a0576bc503ea4ee067b0d4b7e6c12
Juan Diego <juandiego2099@gmail.com>
2026-09-10 11:51:51 -0500

    Activity 2

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
@@ -35,11 +37,6 @@ app.get('/', async (req, res) => {
     res.render('home', { title });
 });
 
-app.get('/organizations', async (req, res) => {
-    const title = 'Our Partner Organizations';
-    res.render('organizations', { title });
-});
-
 app.get('/projects', async (req, res) => {
     const title = 'Service Projects';
     res.render('projects', { title });
@@ -50,9 +47,20 @@ app.get('/categories', async (req, res) => {
     res.render('categories', { title });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://127.0.0.1:${PORT}`);
-  console.log(`Environment: ${NODE_ENV}`);
+app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
+    const title = 'Our Partner Organizations';
+
+    res.render('organizations', { title, organizations });
 });
 
+app.listen(PORT, async () => {
+  try {
+    await testConnection();
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
+});
 
